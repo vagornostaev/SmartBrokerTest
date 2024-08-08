dd641009038147794caa89c24be44d6edb75f9f4
Vadim <vagornostaev@mail.ru>
2024-08-08 18:09:25 +0300

    Initial Commit

---
+++ SmartBrokerTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartBrokerTest
+//
+//  Created by Вадим Горностаев on 08.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartBrokerTest/SmartBrokerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartBrokerTestApp.swift
+//  SmartBrokerTest
+//
+//  Created by Вадим Горностаев on 08.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartBrokerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
